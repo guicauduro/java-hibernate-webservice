9d925638bfb18a9a3a39dffa504f1c9df3225f19
Guilherme Cauduro <guilhermekof@hotmail.com>
2022-05-07 12:59:16 -0400

    Order, Instant, ISO 8601 p1

--- src/main/java/com/guilherme/javahibernatews/entities/User.java
+++ src/main/java/com/guilherme/javahibernatews/entities/User.java
@@ -1,14 +1,19 @@
 package com.guilherme.javahibernatews.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user") 
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -21,6 +26,11 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	// Associacoes
+	@OneToMany(mappedBy = "client") //Nome do atributo do outro lado da associacao
+	private List<Order> orders = new ArrayList<>();
+	
+	// Construtores
 	public User() {
 	}
 
@@ -72,6 +82,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -89,7 +103,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 	
 }
