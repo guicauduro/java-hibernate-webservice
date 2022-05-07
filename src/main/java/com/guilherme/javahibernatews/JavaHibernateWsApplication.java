cf65fb0a60c83eff46a9c3ecfadeb6bdb2c2d333
Guilherme Cauduro <guilhermekof@hotmail.com>
2022-05-07 11:26:40 -0300

    fist

---
+++ src/main/java/com/guilherme/javahibernatews/JavaHibernateWsApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.javahibernatews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHibernateWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHibernateWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/javahibernatews/JavaHibernateWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.javahibernatews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHibernateWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
